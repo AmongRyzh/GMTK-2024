e002c32b5f2dafaf2968d5cbbe1b0dddaf14b695
AmongRyzh <sasharyzh@icloud.com>
2024-08-16 21:50:14 +0300

    Сделал передвижение игрока и верхние кнопки

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float regularMoveSpeed, highMoveSpeed;
+    private Rigidbody2D rb;
+
+    private enum SpeedState
+    {
+        Paused = 0,
+        RegularSpeed = 1,
+        HighSpeed = 2
+    }
+    private SpeedState speedState;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        speedState = SpeedState.RegularSpeed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (speedState == SpeedState.RegularSpeed || speedState == SpeedState.HighSpeed)
+            {
+                speedState = SpeedState.Paused;
+            }
+            else
+            {
+                speedState = SpeedState.RegularSpeed;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            speedState = speedState == SpeedState.HighSpeed ? SpeedState.RegularSpeed : SpeedState.HighSpeed;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartLevel();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (speedState == SpeedState.Paused) return;
+
+        float moveSpeed = speedState == SpeedState.RegularSpeed ? regularMoveSpeed : highMoveSpeed;
+
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+
+    public void SelectSpeedState(int stateID)
+    {
+        speedState = (SpeedState)stateID;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
